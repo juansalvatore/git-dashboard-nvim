26640d204fb1673cb468aaac07fcc586a7cba1c3
Juan Salvatore <juansalvatore.ar@gmail.com>
2024-06-21 19:51:58 -0300

    Improve highlight creation code

--- lua/git-dashboard-nvim/heatmap/utils.lua
+++ lua/git-dashboard-nvim/heatmap/utils.lua
@@ -38,6 +38,65 @@ HeatmapUtils.generate_base_heatmap = function(dates, current_date_info)
   return heatmap
 end
 
+---@param group_name string
+---@param match string
+---@param fg_color string
+HeatmapUtils.add_highlight_group = function(group_name, match, fg_color)
+  vim.cmd("highlight " .. group_name .. " guifg=" .. fg_color)
+  vim.cmd('call matchadd("' .. group_name .. '", "' .. match .. '")')
+end
+
+---@param config Config
+---@param current_date_info table
+---@param branch_label string
+---@param title string
+HeatmapUtils.add_highlights = function(config, current_date_info, branch_label, title)
+  -- Clear existing matches
+  vim.cmd("match none")
+
+  -- clear dashboard header existing highlights
+  vim.cmd("highlight clear DashboardHeader")
+
+  HeatmapUtils.add_highlight_group("DashboardTitle", title, config.colors.dashboard_title)
+
+  HeatmapUtils.add_highlight_group(
+    "EmptySquareHighlight",
+    config.empty_square,
+    config.colors.empty_square_highlight
+  )
+
+  HeatmapUtils.add_highlight_group(
+    "FilledSquareHighlight",
+    config.filled_square,
+    config.colors.filled_square_highlight
+  )
+
+  HeatmapUtils.add_highlight_group("BranchHighlight", branch_label, config.colors.branch_highlight)
+
+  for i = 1, #config.days do
+    HeatmapUtils.add_highlight_group(
+      "DayHighlight",
+      config.days[i],
+      config.colors.days_and_months_labels
+    )
+  end
+
+  for i = 1, current_date_info.current_month do
+    HeatmapUtils.add_highlight_group(
+      "MonthHighlight",
+      config.months[i],
+      config.colors.days_and_months_labels
+    )
+  end
+
+  -- on dashboard buffer leave remove all the above highlights
+  vim.cmd.autocmd(
+    "BufLeave",
+    "*",
+    "highlight clear DashboardHeader | highlight clear EmptySquareHighlight | highlight clear FilledSquareHighlight | highlight clear BranchHighlight | highlight clear DashboardTitle | highlight clear DayHighlight | highlight clear MonthHighlight"
+  )
+end
+
 ---@param base_heatmap number[][]
 ---@param config Config
 ---@param repo_with_owner string
@@ -49,6 +108,7 @@ HeatmapUtils.generate_ascii_heatmap = function(
   current_date_info
 )
   local ascii_heatmap = ""
+  local branch_label = " " .. config.branch
 
   local title = ""
   if config.title == "owner_with_repo_name" then
@@ -63,7 +123,8 @@ HeatmapUtils.generate_ascii_heatmap = function(
     ascii_heatmap = ascii_heatmap .. "\n\n"
   end
 
-  vim.cmd('call matchadd("DashboardTitle", "' .. title .. '")')
+  -- add highlights to the heatmap based on the config settings
+  HeatmapUtils.add_highlights(config, current_date_info, branch_label, title)
 
   if #config.gap == 1 then
     ascii_heatmap = ascii_heatmap .. " "
@@ -76,31 +137,18 @@ HeatmapUtils.generate_ascii_heatmap = function(
     ascii_heatmap = ascii_heatmap .. "\n"
   end
 
-  -- Clear existing matches
-  vim.cmd("match none")
-
-  vim.cmd("highlight clear DashboardHeader")
-
   for i = 1, current_date_info.days_in_week do
-    local highlight_group = "DayHighlight" .. i
     ascii_heatmap = ascii_heatmap .. config.days[i] .. config.day_label_gap
-    vim.cmd('call matchadd("' .. highlight_group .. '", "' .. config.days[i] .. '")')
 
     for j = 1, #base_heatmap do
       if
         j == tonumber(current_date_info.current_week)
         and i > tonumber(current_date_info.current_day_of_week) + 1
       then
-        highlight_group = "EmptySquareHighlight"
-        vim.cmd("call matchadd('" .. highlight_group .. "', '" .. config.empty_square .. "')")
         ascii_heatmap = ascii_heatmap .. config.empty .. config.gap
       elseif base_heatmap[j][i] > 0 then
-        highlight_group = "FilledSquareHighlight"
-        vim.cmd("call matchadd('" .. highlight_group .. "', '" .. config.filled_square .. "')")
         ascii_heatmap = ascii_heatmap .. config.filled_square .. config.gap
       else
-        highlight_group = "EmptySquareHighlight"
-        vim.cmd("call matchadd('" .. highlight_group .. "', '" .. config.empty_square .. "')")
         ascii_heatmap = ascii_heatmap .. config.empty_square .. config.gap
       end
     end
@@ -109,24 +157,8 @@ HeatmapUtils.generate_ascii_heatmap = function(
   end
 
   if config.show_current_branch then
-    local highlight_group = "BranchHighlight"
-    local branch_label = " " .. config.branch
     ascii_heatmap = ascii_heatmap .. "\n" .. branch_label .. "\n"
-    vim.cmd('call matchadd("' .. highlight_group .. '",' .. '"' .. branch_label .. '")')
   end
-
-  vim.cmd("highlight DashboardHeader guifg=" .. config.colors.days_and_months_labels)
-  vim.cmd("highlight EmptySquareHighlight guifg=" .. config.colors.empty_square_highlight)
-  vim.cmd("highlight FilledSquareHighlight guifg=" .. config.colors.filled_square_highlight)
-  vim.cmd("highlight BranchHighlight guifg=" .. config.colors.branch_highlight)
-  vim.cmd("highlight DashboardTitle guifg=" .. config.colors.dashboard_title)
-
-  -- on dashboard buffer leave remove all the above highlights
-  vim.cmd.autocmd(
-    "BufLeave",
-    "*",
-    "highlight clear DashboardHeader | highlight clear EmptySquareHighlight | highlight clear FilledSquareHighlight | highlight clear BranchHighlight | highlight clear DashboardTitle"
-  )
 
   return ascii_heatmap
 end
